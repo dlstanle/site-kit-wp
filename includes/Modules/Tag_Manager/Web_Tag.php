f824485048bbf5c48010a72c06b4fbe4f7ea21da
Devon Stanley <dlstanley@google.com>
2024-01-29 21:09:37 +0000

    Fix invalid string placeholder for sprintf.

--- includes/Modules/Tag_Manager/Web_Tag.php
+++ includes/Modules/Tag_Manager/Web_Tag.php
@@ -86,7 +86,7 @@ class Web_Tag extends Module_Web_Tag {
 				var f = d.getElementsByTagName( s )[0],
 					j = d.createElement( s ), dl = l != 'dataLayer' ? '&l=' + l : '';
 				j.async = true;
-				j.src = '%/gtm.js?id=' + i + dl;
+				j.src = '%s/gtm.js?id=' + i + dl;
 				f.parentNode.insertBefore( j, f );
 			} )( window, document, 'script', 'dataLayer', '%s' );
 			",
